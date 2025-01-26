58fe4c8fd2027c0378f80c9d7fa3623e0dc561d7
Ayushmaan Gupta <ayush.261.gupta@gmail.com>
2025-01-26 10:56:39 +0530

    Db added successfully

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectToDB from "./utils/db.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-console.log("PORT from .env:", process.env.PORT);
+console.log("PORT from env:", process.env.PORT);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await connectToDB();
   console.log(`Server is running on port ${port}`);
 });
