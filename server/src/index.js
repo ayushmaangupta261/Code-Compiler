19c38cae631225e82c99230c76d93e8d694d3bf8
ayushmaangupta261 <ayush.261.gupta@gmail.com>
2025-01-27 22:54:59 +0530

    Home routing of backend

--- server/src/index.js
+++ server/src/index.js
@@ -11,9 +11,9 @@ import {app} from "./app.js"
 const port = process.env.PORT || 8000;
 console.log("PORT from env:", process.env.PORT);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// }); 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+}); 
 
 app.listen(port, async () => {
   await connectToDB();
